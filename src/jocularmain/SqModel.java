0c8a548248aa75db00c1e3e7a63f554f8b4c0414
Bob Anderson <bob.anderson.ok@gmail.com>
2014-05-12 22:16:33 -0700

    Adds SqModel counting of baseline and event points

--- src/jocularmain/SqModel.java
+++ src/jocularmain/SqModel.java
@@ -6,13 +6,24 @@ public class SqModel {
 
     private int dTranIndex = Integer.MIN_VALUE;
     private int rTranIndex = Integer.MAX_VALUE;
-    
+
     private double logL = Double.NaN;
     private double dSolution = Double.NaN;
     private double rSolution = Double.NaN;
 
-    public SqModel(Observation obs) {
+    private Observation obs;
 
+    private double cumBaseline;
+    private double cumEvent;
+
+    private double baseline;
+    private double event;
+
+    private int numBaselinePoints = 0;
+    private int numEventPoints = 0;
+
+    public SqModel(Observation obs) {
+        this.obs = obs;
     }
 
     public SqModel setDtransition(int dTranIndex) {
@@ -26,14 +37,79 @@ public class SqModel {
     }
 
     public double calcLogL() {
+        resetCalculation();
+        calculateNumberOfEventAndBaselinePoints();
         return 0.0;
     }
-    
+
+    private void resetCalculation() {
+
+        logL = Double.NaN;
+        dSolution = Double.NaN;
+        rSolution = Double.NaN;
+
+        numBaselinePoints = 0;
+        numEventPoints = 0;
+    }
+
+    private void calculateNumberOfEventAndBaselinePoints() {
+        if (inRange(dTranIndex) && inRange(rTranIndex)) {
+            numEventPoints = rTranIndex - dTranIndex - 1;
+        } else if (inRange(rTranIndex)) {
+            numEventPoints = rTranIndex;
+        } else {
+            numEventPoints = obs.lengthOfDataColumns - dTranIndex - 1;
+        }
+
+        numBaselinePoints = obs.lengthOfDataColumns - numEventPoints;
+        if (inRange(dTranIndex)) {
+            numBaselinePoints--;
+        }
+        if (inRange(rTranIndex)) {
+            numBaselinePoints--;
+        }
+    }
+
+    private boolean inRange(int index) {
+        if (index < 0) {
+            return false;
+        } else {
+            return index < obs.lengthOfDataColumns;
+        }
+    }
+
+    public int getNumEventPoints() {
+        return numEventPoints;
+    }
+
+    public int getNumBaselinePoints() {
+        return numBaselinePoints;
+    }
+
     public double getDsolution() {
         return dSolution;
     }
-    
+
     public double getRsolution() {
         return rSolution;
     }
+
+    private void addToBaseline(double value) {
+        if (numBaselinePoints == 0) {
+            cumBaseline = value;
+        } else {
+            cumBaseline += value;
+        }
+        numBaselinePoints++;
+    }
+
+    private void addToEvent(double value) {
+        if (numEventPoints == 0) {
+            cumEvent = value;
+        } else {
+            cumEvent += value;
+        }
+        numEventPoints++;
+    }
+
 }
--- test/joculartest/SqModelTest.java
+++ test/joculartest/SqModelTest.java
@@ -7,29 +7,138 @@ import utils.Observation;
 import utils.SampleDataGenerator;
 
 public class SqModelTest {
-    
+
     public SqModelTest() {
     }
 
     @Test
-    public void sqmodelCalcLogL_shouldGiveCorrectResult() {
+    public void sqmodel_shouldCountEventAndBaselinePoints_whenDandRinrange() {
         SampleDataGenerator dataGen = new SampleDataGenerator("testSetTwo");
 
         dataGen.setDevent(1.5)
-                .setRevent(4.5)
-                .setSigmaA(1.0)
-                .setSigmaB(1.0)
-                .setAintensity(1.0)
-                .setBintensity(11.0)
-                .setNumDataPoints(8)
-                .setParams();
+            .setRevent(4.5)
+            .setSigmaA(1.0)
+            .setSigmaB(1.0)
+            .setAintensity(1.0)
+            .setBintensity(11.0)
+            .setNumDataPoints(8)
+            .setParams();
 
         Observation specialObservation = dataGen.build();
-        SqModel sqmodel = new SqModel( specialObservation );
-        
+        SqModel sqmodel = new SqModel(specialObservation);
+
         double logLcalculated = sqmodel.setDtransition(2).setRtransition(5).calcLogL();
-        
-        assertThat(logLcalculated).isEqualTo(-12345.6);
+
+        assertThat(sqmodel.getNumEventPoints()).isEqualTo(2);
+        assertThat(sqmodel.getNumBaselinePoints()).isEqualTo(4);
+    }
+    
+    @Test
+    public void sqmodel_shouldCountEventAndBaselinePoints_whenDonEdgeRinrange() {
+        SampleDataGenerator dataGen = new SampleDataGenerator("testSetTwo");
+
+        dataGen.setDevent(1.5)
+            .setRevent(4.5)
+            .setSigmaA(1.0)
+            .setSigmaB(1.0)
+            .setAintensity(1.0)
+            .setBintensity(11.0)
+            .setNumDataPoints(8)
+            .setParams();
+
+        Observation specialObservation = dataGen.build();
+        SqModel sqmodel = new SqModel(specialObservation);
+
+        double logLcalculated = sqmodel.setDtransition(0).setRtransition(5).calcLogL();
+
+        assertThat(sqmodel.getNumEventPoints()).isEqualTo(4);
+        assertThat(sqmodel.getNumBaselinePoints()).isEqualTo(2);
+    }
+    
+    @Test
+    public void sqmodel_shouldCountEventAndBaselinePoints_whenDoutofrangeRinrange() {
+        SampleDataGenerator dataGen = new SampleDataGenerator("testSetTwo");
+
+        dataGen.setDevent(1.5)
+            .setRevent(4.5)
+            .setSigmaA(1.0)
+            .setSigmaB(1.0)
+            .setAintensity(1.0)
+            .setBintensity(11.0)
+            .setNumDataPoints(8)
+            .setParams();
+
+        Observation specialObservation = dataGen.build();
+        SqModel sqmodel = new SqModel(specialObservation);
+
+        double logLcalculated = sqmodel.setDtransition(-2000).setRtransition(5).calcLogL();
+
+        assertThat(sqmodel.getNumEventPoints()).isEqualTo(5);
+        assertThat(sqmodel.getNumBaselinePoints()).isEqualTo(2);
+    }
+    
+    @Test
+    public void sqmodel_shouldCountEventAndBaselinePoints_whenDinrangeRonedge() {
+        SampleDataGenerator dataGen = new SampleDataGenerator("testSetTwo");
+
+        dataGen.setDevent(1.5)
+            .setRevent(4.5)
+            .setSigmaA(1.0)
+            .setSigmaB(1.0)
+            .setAintensity(1.0)
+            .setBintensity(11.0)
+            .setNumDataPoints(8)
+            .setParams();
+
+        Observation specialObservation = dataGen.build();
+        SqModel sqmodel = new SqModel(specialObservation);
+
+        double logLcalculated = sqmodel.setDtransition(2).setRtransition(7).calcLogL();
+
+        assertThat(sqmodel.getNumEventPoints()).isEqualTo(4);
+        assertThat(sqmodel.getNumBaselinePoints()).isEqualTo(2);
+    }
+    
+    @Test
+    public void sqmodel_shouldCountEventAndBaselinePoints_whenDinrangeRoutofrange() {
+        SampleDataGenerator dataGen = new SampleDataGenerator("testSetTwo");
+
+        dataGen.setDevent(1.5)
+            .setRevent(4.5)
+            .setSigmaA(1.0)
+            .setSigmaB(1.0)
+            .setAintensity(1.0)
+            .setBintensity(11.0)
+            .setNumDataPoints(8)
+            .setParams();
+
+        Observation specialObservation = dataGen.build();
+        SqModel sqmodel = new SqModel(specialObservation);
+
+        double logLcalculated = sqmodel.setDtransition(2).setRtransition(2000).calcLogL();
+
+        assertThat(sqmodel.getNumEventPoints()).isEqualTo(5);
+        assertThat(sqmodel.getNumBaselinePoints()).isEqualTo(2);
     }
 
+//    @Test
+//    public void sqmodelCalcLogL_shouldGiveCorrectResult() {
+//        SampleDataGenerator dataGen = new SampleDataGenerator("testSetTwo");
+//
+//        dataGen.setDevent(1.5)
+//                .setRevent(4.5)
+//                .setSigmaA(1.0)
+//                .setSigmaB(1.0)
+//                .setAintensity(1.0)
+//                .setBintensity(11.0)
+//                .setNumDataPoints(8)
+//                .setParams();
+//
+//        Observation specialObservation = dataGen.build();
+//        SqModel sqmodel = new SqModel( specialObservation );
+//        
+//        double logLcalculated = sqmodel.setDtransition(2).setRtransition(5).calcLogL();
+//        
+//        assertThat(logLcalculated).isEqualTo(-12345.6);
+//    }
 }
