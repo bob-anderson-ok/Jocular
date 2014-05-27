16a4f746bd229f3376c4ffb8a284be88d6935507
Bob Anderson <bob.anderson.ok@gmail.com>
2014-05-27 12:06:26 -0700

    Adds getName() to DataType just prior to refactoring the repaintChart() methid in RootViewController

--- src/jocularmain/DataType.java
+++ src/jocularmain/DataType.java
@@ -1,9 +1,19 @@
 package jocularmain;
 
 public enum DataType { 
-    OBSDATA,       // Observation data points
-    SAMPLE,        // Light curve underlying artificially generated sample data
-    SOLUTION,      // Solution light curve
-    SECONDARY,     // Secondary observation data points
-    SUBFRAME_BAND  // Outline of subframe-timing band
+    OBSDATA("ObsData"),       // Observation data points
+    SAMPLE("Sample"),        // Light curve underlying artificially generated sample data
+    SOLUTION("Solution"),      // Solution light curve
+    SECONDARY("Secondary"),     // Secondary observation data points
+    SUBFRAME_BAND("SubframeBand");  // Outline of subframe-timing band
+    
+    private String dataName;
+    
+    DataType(String dataName) {
+        this.dataName = dataName;
+    }
+    
+    public String getName() {
+        return dataName;
+    }
 }
